1f31988652c047448423fe7fff43d6fe8c90d0dd
Chris Krycho <chris@chriskrycho.com>
2016-10-21 21:35:45 -0400

    Start parsing some args. 🎉

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+/// Run the static site generator.
+#[macro_use]
+extern crate clap;
+
+use clap::{Arg, ArgMatches, App, AppSettings, SubCommand};
+
+
+fn cli<'a>() -> ArgMatches<'a> {
+  App::new("SSG(rs)")
+    .setting(AppSettings::ArgRequiredElseHelp)
+    .version("0.1.0")
+    .author(crate_authors!())
+    .about("Meets my peculiar needs for generating static sites... *fast*.")
+    .subcommand(SubCommand::with_name("generate")
+      .about("Generate the site")
+      .arg(Arg::with_name("local")
+        .long("--local")
+        .help("Use local paths to resources")))
+    .get_matches()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+  let matches = cli();
 }
