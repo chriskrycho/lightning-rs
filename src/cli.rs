fe5d469b9a1a693f2effcf22f6cb5c449fd872ce
Chris Krycho <chris@chriskrycho.com>
2016-10-23 16:34:24 -0400

    Iterate toward actually doing useful things with args. :zap:

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,8 @@
 /// Supply the command line.
 
-use clap::{Arg, ArgMatches, App, AppSettings};
+use std::fmt;
+
+use clap::{Arg, ArgMatches, App, AppSettings, SubCommand};
 
 
 // TODO: figure out a way, eventually, to customize arguments based on whatever
@@ -15,8 +17,8 @@ pub fn cli<'a, 'b>(
   args.extend_from_slice(additional_args);
 
   let mut subcommands = vec![
-    commands::generate(),
-    commands::new(),
+    generate(),
+    new(),
   ];
 
   subcommands.extend_from_slice(additional_subcommands);
@@ -37,34 +39,63 @@ pub fn cli<'a, 'b>(
 // e.g. the user (*me*, initially!) can e.g. just specify a location for
 // new templates in the configuration file, drop a template in there, and have
 // the generator pick it up correctly as one of the options.
-mod commands {
-  use clap::{Arg, App, SubCommand};
+pub enum Commands {
+  Generate,
+  New,
+  Unspecified,
+}
 
-  pub fn generate<'a, 'b>() -> App<'a, 'b> {
-    SubCommand::with_name("generate")
-      .about("Generate the site")
-      .arg(Arg::with_name("local")
-        .help("Use local paths to resources")
-        .long("local"))
-      .arg(Arg::with_name("serve")
-        .short("s")
-        .long("serve")
-        .alias("server"))
+impl fmt::Display for Commands {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      &Commands::Generate => write!(f, "generate"),
+      &Commands::New => write!(f, "new"),
+      &Commands::Unspecified => write!(f, "error!!!"),  // TODO: something else!
+    }
   }
+}
 
-  // TODO: different templates will take different args, right?
-  pub fn new<'a, 'b>() -> App<'a, 'b> {
-    SubCommand::with_name("new")
-      .about("Create an item from a template")
-      .arg(Arg::with_name("template")
-        .help("The name of the template to generate, e.g. `post`")
-        .index(1)
-        .required(true)
-        .possible_values(&["post"]))
-      .arg(Arg::with_name("title")
-        .help("The title to use in the template")
-        .index(2)
-        .required(true)
-        .takes_value(true))
+impl<'a> From<&'a str> for Commands {
+  fn from(s: &str) -> Commands {
+    match s {
+      "generate" => Commands::Generate,
+      "new" => Commands::New,
+      _ => Commands::Unspecified
+    }
   }
+}
+
+
+// -- TODO: I feel like the following should be namespaced somehow?
+
+/// Generate the site.
+fn generate<'a, 'b>() -> App<'a, 'b> {
+  SubCommand::with_name("generate")
+    .about("Generate the site")
+    .arg(Arg::with_name("local")
+      .help("Use local paths to resources")
+      .long("local"))
+    .arg(Arg::with_name("serve")
+      .short("s")
+      .long("serve")
+      .alias("server"))
+}
+
+
+/// Generate a new item from a template.
+///
+/// TODO: different templates will take different args, right?
+fn new<'a, 'b>() -> App<'a, 'b> {
+  SubCommand::with_name("new")
+    .about("Create an item from a template")
+    .arg(Arg::with_name("template")
+      .help("The name of the template to generate, e.g. `post`")
+      .index(1)
+      .required(true)
+      .possible_values(&["post"]))
+    .arg(Arg::with_name("title")
+      .help("The title to use in the template")
+      .index(2)
+      .required(true)
+      .takes_value(true))
 }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,28 @@ mod cli;
 
 use clap::{Arg, App};
 
+use cli::Commands;
+
 
 fn main() {
-  let args: Vec<Arg> = vec![];
-  let commands: Vec<App> = vec![];
-  let matches = cli::cli(&args, &commands);
+  let extra_args: Vec<Arg> = vec![];
+  let subcommands: Vec<App> = vec![];
+  let commands = cli::cli(&extra_args, &subcommands);
+
+  if let Some(command_name) = commands.subcommand_name() {
+    match Commands::from(command_name) {
+      Commands::Generate => {},
+      Commands::New => {},
+      Commands::Unspecified => {/* FAIL SOMEHOW */}
+    }
+  }
+
+
+  // TODO:
+  // 0. Determine command.
+  // 1. Load config.
+  // 2. Execute command. So: make functions to dispatch for commands!
+  //     - generate:
+  //         - get list of docs in source directory (from config)
+  //         -
 }
