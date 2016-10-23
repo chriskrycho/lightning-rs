034c18ca92795271f13dae11dada734408beb9c1
Chris Krycho <chris@chriskrycho.com>
2016-10-23 11:23:15 -0400

    Spiffy up cli.rs a bit, clarify some TODOs.

--- src/cli.rs
+++ src/cli.rs
@@ -1,16 +1,33 @@
 /// Supply the command line.
 
-use clap::{Arg, ArgMatches, App, AppSettings, SubCommand};
+use clap::{Arg, ArgMatches, App, AppSettings};
 
 
+// TODO: figure out a way, eventually, to customize arguments based on whatever
+// external tools are supplied---without requiring a rebuild. (Compare what
+// Cargo does.)
 pub fn cli<'a, 'b>(additional_args: &[Arg<'a, 'b>]) -> ArgMatches<'a> {
   App::new("SSG(rs)")
     .setting(AppSettings::ArgRequiredElseHelp)
     .version("0.1.0")
     .author(crate_authors!())
     .about("Meets my peculiar needs for generating static sites... *fast*.")
+    .subcommands(vec![subcommands::generate(), subcommands::new()])
+    .args(additional_args)
+    .get_matches()
+}
 
-    .subcommand(SubCommand::with_name("generate")
+
+// TODO: figure out what the best way to programmatically generate new options
+// for these based on the state of the file system where it's run is, so that
+// e.g. the user (*me*, initially!) can e.g. just specify a location for
+// new templates in the configuration file, drop a template in there, and have
+// the generator pick it up correctly as one of the options.
+mod subcommands {
+  use clap::{Arg, App, SubCommand};
+
+  pub fn generate<'a, 'b>() -> App<'a, 'b> {
+    SubCommand::with_name("generate")
       .about("Generate the site")
       .arg(Arg::with_name("local")
         .help("Use local paths to resources")
@@ -18,27 +35,21 @@ pub fn cli<'a, 'b>(additional_args: &[Arg<'a, 'b>]) -> ArgMatches<'a> {
       .arg(Arg::with_name("serve")
         .short("s")
         .long("serve")
-        .alias("server")))
+        .alias("server"))
+  }
 
-    // TODO: figure out a way, eventually, to customize arguments.
-    //
-    // I can probably just take a list of `Arg`s to `cli()`, and then do
-    // `.args()` with the list.
-    .subcommand(SubCommand::with_name("new")
+  pub fn new<'a, 'b>() -> App<'a, 'b> {
+    SubCommand::with_name("new")
       .about("Create an item from a template")
       .arg(Arg::with_name("template")
         .help("The name of the template to generate, e.g. `post`")
         .index(1)
         .required(true)
-        .possible_values(&["post"])
-      )
+        .possible_values(&["post"]))
       .arg(Arg::with_name("title")
         .help("The title to use in the template")
         .index(2)
         .required(true)
-        .takes_value(true)
-      )
-    )
-    .args(additional_args)
-    .get_matches()
+        .takes_value(true))
+  }
 }
