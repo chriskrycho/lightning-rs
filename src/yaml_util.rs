daffc4728f4eded2480398a3468c5eed51343952
Chris Krycho <chris@chriskrycho.com>
2017-05-28 09:52:33 -0600

    Implement, test case-insensitive YAML string extraction.

--- src/yaml_util.rs
+++ src/yaml_util.rs
@@ -23,24 +23,46 @@ impl fmt::Display for Required {
     }
 }
 
-pub fn case_insensitive_string(lower: &str,
-                               upper: &str,
-                               yaml: &Hash,
-                               required: Required)
-                               -> Result<Option<String>, String> {
-    match yaml.get(&Yaml::from_str(lower))
-              .or(yaml.get(&Yaml::from_str(upper))) {
+/// Extract a key irrespective of the case the user supplied it in.
+///
+/// - `value`: the title-case or lower-case version of the string to match. The
+///   YAML will be checked against both this and the Title-cased version. E.g.
+///   if the value passed is `"hello"`, both `"hello"` and `"Hello"` will be
+///   checked. If the value passed is `"Ahoy"`, both `"ahoy"` and `"Ahoy"` will
+///   be
+/// - `yaml`: a `yaml::Hash` against which to check the value.
+/// - `required`: determines whether to return an *error* if the key is missing,
+///   or to return `Ok(None)`. This lets the caller determine how to handle the
+///   missing-value type as it deems appropriate, including e.g. simply by
+///   letting it be passed up with `?` if `Required::Yes`.
+pub fn case_insensitive_string(
+    value: &str,
+    yaml: &Hash,
+    required: Required,
+) -> Result<Option<String>, String> {
+    let mut chars = value.chars();
+    let first_char = chars.next().ok_or("no key passed")?;
 
-        Some(&Yaml::String(ref string)) => Ok(Some(string.clone())),
+    // TODO: can I avoid allocating a new `String` for each of these?
+    let upper = format!("{}{}", first_char.to_uppercase(), chars.as_str());
+    let lower = format!("{}{}", first_char.to_lowercase(), chars.as_str());
+    let upper_key = Yaml::from_str(&upper);
+    let lower_key = Yaml::from_str(&lower);
 
+    match yaml.get(&upper_key).or(yaml.get(&lower_key)) {
+        Some(&Yaml::String(ref value)) => Ok(Some(value.clone())),
         _ => {
             match required {
                 Required::No => Ok(None),
                 Required::Yes => {
-                    Err(key_of_type(format!("{} (case insensitive)", lower).as_str(),
-                                    required,
-                                    yaml,
-                                    "string"))
+                    Err(
+                        key_of_type(
+                            format!("{} (case insensitive)", value).as_str(),
+                            required,
+                            yaml,
+                            "string",
+                        )
+                    )
                 }
             }
         }
@@ -51,32 +73,72 @@ pub fn required_key<Y: fmt::Debug>(key: &str, yaml: &Y) -> String {
     format!("Required key `{}` missing from {:?}", key, yaml)
 }
 
-pub fn key_of_type<Y: fmt::Debug>(key: &str,
-                                  required: Required,
-                                  yaml: Y,
-                                  required_type: &str)
-                                  -> String {
-    format!("{} key `{}` in {:?} must be a {}",
-            required,
-            key,
-            yaml,
-            required_type)
+pub fn key_of_type<Y: fmt::Debug>(
+    key: &str,
+    required: Required,
+    yaml: Y,
+    required_type: &str,
+) -> String {
+    format!(
+        "{} key `{}` in {:?} must be a {}",
+        required,
+        key,
+        yaml,
+        required_type
+    )
 }
 
 pub fn bad_value<V: fmt::Debug, Y: fmt::Debug>(value: V, key: &str, context: &Y) -> String {
-    format!("Invalid value {:?} for key `{}` in hash {:?}",
-            value,
-            key,
-            context)
+    format!(
+        "Invalid value {:?} for key `{}` in hash {:?}",
+        value,
+        key,
+        context
+    )
 }
 
-pub fn ridiculous_number<V: fmt::Display + ops::Add>(value: V,
-                                                     key: &str,
-                                                     context: &Hash)
-                                                     -> String {
-    format!("Seriously? You set the value of `{}` to {}? (The max is {}.)\nContext: {:?}",
-            key,
-            value,
-            u8::MAX,
-            context)
+pub fn ridiculous_number<V: fmt::Display + ops::Add>(
+    value: V,
+    key: &str,
+    context: &Hash,
+) -> String {
+    format!(
+        "Seriously? You set the value of `{}` to {}? (The max is {}.)\nContext: {:?}",
+        key,
+        value,
+        u8::MAX,
+        context
+    )
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_insensitive_string_handles_title_case() {
+        let value = "the value";
+        let mut yaml = Hash::new();
+        yaml.insert(Yaml::from_str("Title"), Yaml::from_str(value));
+
+        let lower = case_insensitive_string("title", &yaml, Required::No);
+        assert_eq!(lower, Ok(Some(value.to_string())));
+
+        let title = case_insensitive_string("Title", &yaml, Required::No);
+        assert_eq!(title, Ok(Some(value.to_string())));
+    }
+
+    #[test]
+    fn case_insensitive_string_handles_lower_case() {
+        let value = "the value";
+        let mut yaml = Hash::new();
+        yaml.insert(Yaml::from_str("title"), Yaml::from_str(value));
+
+        let lower = case_insensitive_string("title", &yaml, Required::No);
+        assert_eq!(lower, Ok(Some(value.to_string())));
+
+        let title = case_insensitive_string("Title", &yaml, Required::No);
+        assert_eq!(title, Ok(Some(value.to_string())));
+    }
 }
