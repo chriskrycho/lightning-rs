5064ae4149cd84b75fd9ced646241319779ed185
Chris Krycho <hello@chriskrycho.com>
2020-12-20 22:16:13 -0700

    Associate path with contents when loading data

--- src/build.rs
+++ src/build.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 
 use json5;
 
@@ -11,18 +11,19 @@ pub fn build(in_dir: PathBuf) -> Result<(), String> {
     let config: Config = json5::from_str(&data)
         .map_err(|e| format!("could not parse '{}':\n{}", &data_file.display(), e))?;
 
-    dbg!("{}", config);
-
     let content_dir = in_dir.join("content");
     let content_glob = content_dir.to_string_lossy() + "/**/*.md";
-    let all_contents: Vec<Result<String, String>> = glob::glob(&content_glob)
+    let all_contents: Vec<Result<(PathBuf, String), String>> = glob::glob(&content_glob)
         .expect(&format!("bad glob: '{}'", &content_glob))
         .map(|result| {
-            result
-                .map_err(|e| format!("{}", e))
-                .and_then(|file| std::fs::read_to_string(file).map_err(|e| format!("{}", e)))
+            result.map_err(|e| format!("{}", e)).and_then(|file| {
+                std::fs::read_to_string(&file)
+                    .map(|content| (file, content))
+                    .map_err(|e| format!("{}", e))
+            })
         })
         .collect();
-    println!("{}", all_contents.len());
+
+    dbg!("{}", &all_contents[0]);
     Ok(())
 }
