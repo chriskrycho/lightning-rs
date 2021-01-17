afc82f229efe0d50ff02c133ce50ae720d920b21
Chris Krycho <hello@chriskrycho.com>
2021-01-16 21:05:57 -0700

    Hey, `main` can use `Result` since forever now!

--- src/bin/lx/main.rs
+++ src/bin/lx/main.rs
@@ -2,25 +2,9 @@
 
 mod cli;
 
-// Standard library
-use std::io::prelude::*;
-
-// First party
 use crate::cli::Command;
 
-fn main() {
-    if let Err(reason) = run() {
-        // if this fails, we literally can't do a thing except panic.
-        write!(std::io::stderr(), "failure: {}", reason).unwrap();
-        std::process::exit(1);
-    }
-}
-
-/// Define a `Result`-returning function to run the app.
-///
-/// (This is a standard Rust pattern to support the use of `try~`/`?`. We're
-/// not doing that yet, but I expect we might eventually; this is convenient.)
-fn run() -> Result<(), String> {
+fn main() -> Result<(), String> {
     let cwd = std::env::current_dir()
         .expect("Something is suuuuper borked: I cannot even get the current working directory!");
 
