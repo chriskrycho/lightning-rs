a58ba8b4d7d1162377f2d10f7ffde2acb495d586
Chris Krycho <hello@chriskrycho.com>
2021-01-23 21:31:30 -0700

    Better/nicer config::email setup and deserialization

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,10 @@
+mod email;
+
 use std::path::{Path, PathBuf};
 
 use serde_derive::Deserialize;
+
+use email::Email;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
@@ -40,54 +44,7 @@ pub struct Title {
 #[derive(Deserialize, Debug)]
 pub struct Author {
     name: String,
-    #[serde(deserialize_with = "email::de_from_str")]
-    email: email::Email,
+    #[serde(deserialize_with = "Email::de_from_str")]
+    email: Email,
     links: Vec<String>,
-}
-
-mod email {
-    use std::str::FromStr;
-
-    use lazy_static::lazy_static;
-    use regex::Regex;
-    use serde::{de, Deserialize, Deserializer};
-    use serde_derive::Deserialize;
-
-    lazy_static! {
-        /// An incredibly stupid email-"parsing" regex.
-        static ref EMAIL_RE: Regex = Regex::new(r"([^@]+)@([^@]+)").unwrap();
-    }
-
-    #[derive(Deserialize, Debug)]
-    pub(super) struct Email {
-        local: String,
-        host: String,
-    }
-
-    pub(super) fn de_from_str<'de, D>(deserializer: D) -> Result<Email, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        Email::from_str(&s).map_err(de::Error::custom)
-    }
-
-    impl std::str::FromStr for Email {
-        type Err = String;
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            EMAIL_RE
-                .captures(s)
-                .ok_or(format!("could not parse {}", s))
-                .and_then(|captures| match (captures.get(0), captures.get(1)) {
-                    (Some(local), Some(host)) => Ok(Email {
-                        local: local.as_str().to_owned(),
-                        host: host.as_str().to_owned(),
-                    }),
-                    (Some(..), None) => Err(format!("missing host name in {}", s)),
-                    (None, Some(..)) => Err(format!("missing username in {}", s)),
-                    _ => Err(format!("could not parse {}", s)),
-                })
-                .map_err(|e| format!("email validation error: {}", e))
-        }
-    }
 }
---
+++ src/config/email.rs
@@ -0,0 +1,46 @@
+use std::str::FromStr;
+
+use lazy_static::lazy_static;
+use regex::Regex;
+use serde::{de, Deserialize, Deserializer};
+use serde_derive::Deserialize;
+
+lazy_static! {
+    /// An incredibly stupid email-"parsing" regex.
+    static ref EMAIL_RE: Regex = Regex::new(r"([^@]+)@([^@]+)").unwrap();
+}
+
+#[derive(Deserialize, Debug)]
+pub(super) struct Email {
+    local: String,
+    host: String,
+}
+
+impl Email {
+    pub(super) fn de_from_str<'de, D>(deserializer: D) -> Result<Email, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Email::from_str(&s).map_err(de::Error::custom)
+    }
+}
+
+impl std::str::FromStr for Email {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        EMAIL_RE
+            .captures(s)
+            .ok_or(format!("could not parse {}", s))
+            .and_then(|captures| match (captures.get(0), captures.get(1)) {
+                (Some(local), Some(host)) => Ok(Email {
+                    local: local.as_str().to_owned(),
+                    host: host.as_str().to_owned(),
+                }),
+                (Some(..), None) => Err(format!("missing host name in {}", s)),
+                (None, Some(..)) => Err(format!("missing username in {}", s)),
+                _ => Err(format!("could not parse {}", s)),
+            })
+            .map_err(|e| format!("email validation error: {}", e))
+    }
+}
