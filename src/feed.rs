7676fa4a9fe19bedd121055b04fd601609c36b79
Chris Krycho <hello@chriskrycho.com>
2021-03-06 17:11:26 -0700

    Feeds: include site_config in feed client

--- src/feed.rs
+++ src/feed.rs
@@ -2,12 +2,17 @@ mod json;
 
 use json_feed::JSONFeed;
 
-use crate::page::Page;
+use crate::{config::Config, page::Page};
 
 /// Required resources for a `Feed`.
 pub(crate) struct Feed<'a> {
     /// Every feed has its own title.
     title: String,
+
+    /// Feeds also need read access to the site config to be able to render the
+    /// full set of data specified for Atom, JSON, or RSS.
+    site_config: &'a Config,
+
     /// The set of items to render in the feed. A read-only slice because I will
     /// never actually need to *write* to these. I just need the parsed metadata
     /// and rendered HTML contents of the page, to render into the template.
@@ -15,8 +20,12 @@ pub(crate) struct Feed<'a> {
 }
 
 impl<'a> Feed<'a> {
-    pub(crate) fn new(title: String, items: &'a [Page]) -> Feed<'a> {
-        Feed { title, items }
+    pub(crate) fn new(title: String, site_config: &'a Config, items: &'a [Page]) -> Feed<'a> {
+        Feed {
+            title,
+            site_config,
+            items,
+        }
     }
 }
 
